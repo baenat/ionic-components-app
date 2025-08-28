f860564dc877ab447877c66ba97bb4261efa7dd4
Julian Baena <julianbaena@periferia-it.com>
2025-08-28 14:49:17 -0500

    Implementa menú lateral dinámico con opciones obtenidas desde el servicio de menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { MenuOpts } from './util/interfaces/menu-opts';
+import { MenuService } from './services/menu/menu.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  menuOpts: MenuOpts[] = [];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menuService: MenuService
   ) {
     this.initializeApp();
+    this.getMenuOpts();
   }
 
   initializeApp() {
@@ -23,5 +30,9 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  async getMenuOpts() {
+    this.menuOpts = await this.menuService.getMenuOptions()
   }
 }
